2cd2eafc071f5b52d8b6fcc07a1657898e19e5d3
odilonv <odilon.vidal@icloud.com>
2025-01-19 23:33:14 +0100

    Add initial project setup with Babel, ESLint, and basic server structure

---
+++ api/app/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  development: {
+    type: 'development',
+    port: 3000,
+    mongodb: 'mongodb+srv://odilon:odilon@cluster0.2anko.mongodb.net/mines'
+  },
+  production: {
+    type: 'production',
+    port: 3000,
+    mongodb: 'mongodb+srv://odilon:odilon@cluster0.2anko.mongodb.net/mines'
+  }
+};
